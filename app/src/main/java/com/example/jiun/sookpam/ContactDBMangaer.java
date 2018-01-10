29b3e3e36768cd26b26c5637a5a1fc5cfc79d937
lyn <lync2846@gmail.com>
2018-01-10 17:47:42 +0900

    Refactor name

--- app/src/main/java/com/example/jiun/sookpam/ContactDBMangaer.java
+++ app/src/main/java/com/example/jiun/sookpam/ContactDBMangaer.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -13,19 +12,15 @@ import io.realm.RealmQuery;
 import io.realm.RealmResults;
 
 
-public class Department extends Application {
-    private  static String TAG = "TOTAL";
+public class ContactDBMangaer extends Application {
+    private static String TAG = "TOTAL";
     private Realm realm;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
-        // Get a Realm instance for this thread
-        // The RealmConfiguration is created using the builder pattern.
-
-        // Use the config
-       realm = Realm.getDefaultInstance();
+        realm = Realm.getDefaultInstance();
 
         //adding to db satrt
         if (!(realm.isEmpty())) {
@@ -45,18 +40,17 @@ public class Department extends Application {
                         //ContactRecord user = bgRealm.createObject(ContactRecord.class);
                         while ((line = br.readLine()) != null) {
                             // use comma as separator
-                            ContactRecord user = bgRealm.createObject(ContactRecord.class);
-                            final String[] record = line.split(cvsSplitBy);
-                            user.setClass1(record[0]);
-                            user.setClass2(record[1]);
-                            user.setPhone(record[2]);
+                            ContactRecord record = bgRealm.createObject(ContactRecord.class);
+                            final String[] value = line.split(cvsSplitBy);
+                            record.setClass1(value[0]);
+                            record.setClass2(value[1]);
+                            record.setPhone(value[2]);
                             //Log.i(TAG,user.getClass1()+"/"+user.getClass2()+"/"+user.getPhone()); //test 코드
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
                         Log.i(TAG, "file Not found wrong directory");
-                    }
-                    finally {
+                    } finally {
                         if (br != null) {
                             try {
                                 br.close();
@@ -69,11 +63,7 @@ public class Department extends Application {
             }, new Realm.Transaction.OnSuccess() {
                 @Override
                 public void onSuccess() {
-                    RealmResults<ContactRecord> results = realm.where(ContactRecord.class).findAll();
-                    Log.v("SUCCESS", "size : "+results.size());
-                    for (ContactRecord record : results) {
-                        Log.v(TAG,record .getClass1()+"/"+record .getClass2()+"/"+record .getPhone()); //test 코드
-                    }
+                    printLogInSuccess();
                 }
             }, new Realm.Transaction.OnError() {
                 @Override
@@ -86,5 +76,15 @@ public class Department extends Application {
         }
 
     }
+
+    private void printLogInSuccess() {
+        RealmResults<ContactRecord> results = realm.where(ContactRecord.class).findAll();
+        Log.v("SUCCESS", "size : " + results.size());
+        for (ContactRecord record : results) {
+            Log.v(TAG, record.getClass1() + "/" + record.getClass2() + "/" + record.getPhone()); //test 코드
+        }
+    }
+
+
 
 }
