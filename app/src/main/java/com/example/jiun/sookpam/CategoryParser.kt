753226114ccf548a74eac7201d6fd73e0f0813b6
lyn <lync2846@gmail.com>
2018-01-14 23:21:18 +0900

    add CategoryPaser for parsing the category

--- app/src/main/java/com/example/jiun/sookpam/CategoryParser.kt
+++ app/src/main/java/com/example/jiun/sookpam/CategoryParser.kt
@@ -1,16 +1,38 @@
 package com.example.jiun.sookpam
 
+import android.content.Context
 import android.util.Log
 import com.example.jiun.sookpam.model.data.SmsVO
+import com.example.jiun.sookpam.model.sms.SmsList
 import io.realm.Realm
-import io.realm.RealmResults
-import io.realm.kotlin.where
 
 class CategoryParser {
     private var realm: Realm = Realm.getDefaultInstance()
 
-    public fun categorizeSMS (){
-        var smsList:RealmResults<SmsVO> = realm.where<SmsVO>().findAll()
-        smsList.forEach { sms-> Log.v("SMS",sms.phoneNumber) }
+     fun categorizeSMS (context: Context){
+        var smsList = SmsList().getSmsList()
+         Log.v("SIZE","smsList size : "+smsList.size)
+        for (sms in smsList) {
+            createCategory(sms)
+        }
+         Log.v("Category",realm.where(CategoryVO::class.java).findFirst()?.category)
+    }
+
+    fun createCategory(sms:SmsVO) {
+        realm.executeTransaction { realm ->
+            var categoryRecord: CategoryVO = realm.createObject(CategoryVO::class.java)
+            val category : String = getCategory(sms.phoneNumber)
+            categoryRecord.category = category
+            categoryRecord.sms = sms
+        }
+    }
+
+    fun getCategory(value : String) : String {
+        val record = realm!!.where(ContactVO::class.java).equalTo("phone",value).findFirst()
+        Log.v("PHONE",value+"/"+record!!.phone)
+        if(record == null)
+            return "학교"
+        else
+            return record.class2
     }
 }
