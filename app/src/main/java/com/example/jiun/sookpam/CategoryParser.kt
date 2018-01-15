e81fcb948b614541cd834fd69cf818f019736674
lyn <lync2846@gmail.com>
2018-01-15 10:33:38 +0900

    add MMS parser

--- app/src/main/java/com/example/jiun/sookpam/CategoryParser.kt
+++ app/src/main/java/com/example/jiun/sookpam/CategoryParser.kt
@@ -2,35 +2,57 @@ package com.example.jiun.sookpam
 
 import android.content.Context
 import android.util.Log
+import com.example.jiun.sookpam.model.data.MmsVO
 import com.example.jiun.sookpam.model.data.SmsVO
+import com.example.jiun.sookpam.model.mms.MmsList
 import com.example.jiun.sookpam.model.sms.SmsList
 import io.realm.Realm
 
 class CategoryParser {
     private var realm: Realm = Realm.getDefaultInstance()
 
-     fun categorizeSMS (context: Context){
-        var smsList = SmsList().getSmsList()
-         Log.v("SIZE","smsList size : "+smsList.size)
-        for (sms in smsList) {
-            createCategory(sms)
-        }
-         Log.v("Category",realm.where(CategoryVO::class.java).findFirst()?.category)
+    fun categorizeMessages(context: Context){
+        categorizeSMS()
+        categorizeMMS()
     }
 
-    fun createCategory(sms:SmsVO) {
+    fun categorizeSMS() {
+        var smsList = SmsList().getSmsList()
+        Log.v("SIZE", "smsList size : " + smsList.size)
+        for (sms in smsList)
+            createSMSCategory(sms)
+
+        Log.v("Category", realm.where(CategoryVO::class.java).findFirst()?.category)
+    }
+
+    fun createSMSCategory(sms: SmsVO) {
         realm.executeTransaction { realm ->
             var categoryRecord: CategoryVO = realm.createObject(CategoryVO::class.java)
-            val category : String = getCategory(sms.phoneNumber)
+            val category: String? = getCategory(sms.phoneNumber)
             categoryRecord.category = category
             categoryRecord.sms = sms
         }
     }
 
-    fun getCategory(value : String) : String {
-        val record = realm!!.where(ContactVO::class.java).equalTo("phone",value).findFirst()
-        Log.v("PHONE",value+"/"+record!!.phone)
-        if(record == null)
+    fun categorizeMMS() {
+        var mmsList = MmsList().getMmsList()
+        for (mms in mmsList)
+            createMMSCategory(mms)
+    }
+
+    fun createMMSCategory(mms: MmsVO) {
+        realm.executeTransaction { realm ->
+            var categoryRecord: CategoryVO = realm.createObject(CategoryVO::class.java)
+            val category: String? = getCategory(mms.phoneNumber)
+            categoryRecord.category = category
+            categoryRecord.mms = mms
+        }
+    }
+
+    fun getCategory(value: String?): String {
+        val record = realm!!.where(ContactVO::class.java).equalTo("phone", value).findFirst()
+        Log.v("PHONE", value + "/" + record!!.phone)
+        if (record == null)
             return "학교"
         else
             return record.class2
