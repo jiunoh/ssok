799ea3bea8cf505696d39140ec6b7cc57f86419c
lyn <lync2846@gmail.com>
2018-01-15 10:32:46 +0900

    changed to nullable for some cases(have to ask hyemin)

--- app/src/main/java/com/example/jiun/sookpam/model/data/SmsVO.kt
+++ app/src/main/java/com/example/jiun/sookpam/model/data/SmsVO.kt
@@ -7,7 +7,7 @@ import java.util.*
 open class SmsVO : RealmObject() {
     @PrimaryKey
     var id: Long = 0
-    lateinit var phoneNumber : String
+    var phoneNumber: String? = null
     var date: Date? = null
     var body: String? = null
 }
