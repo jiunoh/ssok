417c2395780ca3b11d5d389decd41e09e8a92abe
Hyemin Kim <1117hyemin@gmail.com>
2018-01-16 00:44:03 +0900

    Create MessageContract class

---
+++ app/src/main/java/com/example/jiun/sookpam/message/MessageContract.kt
@@ -0,0 +1,19 @@
+package com.example.jiun.sookpam.message
+
+import com.example.jiun.sookpam.BasePresenter
+import com.example.jiun.sookpam.BaseView
+import com.gun0912.tedpermission.PermissionListener
+
+interface MessageContract {
+    interface View : BaseView<Presenter> {
+        fun showPermissionMessage(permissionListener: PermissionListener)
+
+        fun finishActivity()
+    }
+
+    interface Presenter : BasePresenter {
+        fun readMessageList()
+
+        fun checkPermission()
+    }
+}
