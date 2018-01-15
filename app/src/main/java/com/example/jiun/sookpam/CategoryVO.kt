2c5240305c02b6cd8e50ff867f8e2442a94ba557
lyn <lync2846@gmail.com>
2018-01-15 10:33:20 +0900

    add mmsLink to CategoryVO

--- app/src/main/java/com/example/jiun/sookpam/CategoryVO.kt
+++ app/src/main/java/com/example/jiun/sookpam/CategoryVO.kt
@@ -1,11 +1,13 @@
 package com.example.jiun.sookpam
 
+import com.example.jiun.sookpam.model.data.MmsVO
 import com.example.jiun.sookpam.model.data.SmsVO
 import io.realm.RealmModel
 import io.realm.annotations.RealmClass
 
 @RealmClass
 open class CategoryVO : RealmModel {
-    lateinit var category : String
-    var sms:SmsVO ?=null
+    var category : String ?=null
+    var sms: SmsVO? = null
+    var mms: MmsVO? = null
 }
