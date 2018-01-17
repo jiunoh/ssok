ee88e6b22c7114531242b7133d4ffa0828b991f2
lyn <lync2846@gmail.com>
2018-01-17 10:14:25 +0900

    remove no needed code

--- app/src/main/java/com/example/jiun/sookpam/MainActivity.kt
+++ app/src/main/java/com/example/jiun/sookpam/MainActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -50,7 +49,6 @@ class MainActivity : AppCompatActivity() {
     private fun scatterCheckedCategories() {
         val contactDBManager = applicationContext as ContactDBManager
         val categoryList = contactDBManager.getCategoryList()
-        adapter.clear()
         for (category in categoryList) {
             if (SharedPreferenceUtil.get(applicationContext, category, false))
                 adapter.addItem(ContextCompat.getDrawable(this, R.drawable.arrow), category)
