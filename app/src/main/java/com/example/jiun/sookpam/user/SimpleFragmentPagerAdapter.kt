891a5c0082237c7cced45963d6eefd8251097fc5
Hyemin Kim <1117hyemin@gmail.com>
2018-02-13 19:09:06 +0900

    Create pager adapter

--- app/src/main/java/com/example/jiun/sookpam/user/SimpleFragmentPagerAdapter.kt
+++ app/src/main/java/com/example/jiun/sookpam/user/SimpleFragmentPagerAdapter.kt
@@ -2,20 +2,22 @@ package com.example.jiun.sookpam.user
 
 import android.support.v4.app.*
 
-internal class SimpleFragmentPagerAdapter(fm: FragmentManager, private val mNumbOfTabs: Int) : FragmentPagerAdapter(fm) {
+internal class SimpleFragmentPagerAdapter(fm: FragmentManager, private val numberOfPages: Int) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment? {
+        if (position < 0 || position >= numberOfPages)
+            return null
         return when (position) {
-            USER_INFO_1 -> UserInfo1Fragment()
-            USER_INFO_2 -> UserInfo2Fragment()
-            USER_INFO_3 -> UserInfo3Fragment()
-            USER_INFO_4 -> UserInfo4Fragment()
-            else -> null
+                USER_INFO_1 -> UserInfo1Fragment()
+                USER_INFO_2 -> UserInfo2Fragment()
+                USER_INFO_3 -> UserInfo3Fragment()
+                USER_INFO_4 -> UserInfo4Fragment()
+                else -> null
         }
     }
 
     override fun getCount(): Int {
-        return mNumbOfTabs
+        return numberOfPages
     }
 
     companion object {
