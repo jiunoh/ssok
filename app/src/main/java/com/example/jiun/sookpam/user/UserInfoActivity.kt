3fa76023818df3d766e3d413cd7adc3de1bd549c
Hyemin Kim <1117hyemin@gmail.com>
2018-02-17 15:25:57 +0900

    Add image actions when user move page

--- app/src/main/java/com/example/jiun/sookpam/user/UserInfoActivity.kt
+++ app/src/main/java/com/example/jiun/sookpam/user/UserInfoActivity.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.view.ViewPager
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import com.example.jiun.sookpam.R
 import kotlinx.android.synthetic.main.activity_user_info.*
 
@@ -14,6 +15,7 @@ class UserInfoActivity : AppCompatActivity() {
     private lateinit var currentFragment: Fragment
     private lateinit var previousButton: Button
     private lateinit var nextButton: Button
+    private var circleImageViewArrayList: ArrayList<ImageView> = ArrayList(3)
     private var pagerAdapter = SimpleFragmentPagerAdapter(supportFragmentManager, MAX_PAGE_SIZE)
     private var currentPage = SimpleFragmentPagerAdapter.USER_INFO_1
 
@@ -25,29 +27,58 @@ class UserInfoActivity : AppCompatActivity() {
     }
 
     private fun initialize() {
+        initializeViewPager()
+        initializeMoveButtons()
+        initializeCircleImages()
+    }
+
+    private fun initializeViewPager() {
         viewPager = user_view_pager
         viewPager.apply {
             this.adapter = pagerAdapter
             currentFragment = pagerAdapter.getItem(currentPage)
         }
+    }
+
+    private fun initializeMoveButtons() {
         previousButton = user_info_previous_btn
         nextButton = user_info_next_btn
     }
 
+    private fun initializeCircleImages() {
+        (0..3)
+                .map {
+                    resources.getIdentifier("user_info_circle" + it + "_img", "id"
+                            , packageName)
+                }
+                .forEach { circleImageViewArrayList.add(findViewById(it)) }
+    }
+
     private fun movePage() {
+        movePrevious()
+        moveNext()
+    }
+
+    private fun movePrevious() {
         previousButton.setOnClickListener {
-            if (currentPage == SimpleFragmentPagerAdapter.USER_INFO_2) {
-                previousButton.visibility = View.INVISIBLE
+            if (currentPage == SimpleFragmentPagerAdapter.USER_INFO_3) {
+                nextButton.text = getText(R.string.user_info_next_page)
             }
             if (currentPage > SimpleFragmentPagerAdapter.USER_INFO_1) {
+                if (currentPage == SimpleFragmentPagerAdapter.USER_INFO_2) {
+                    previousButton.visibility = View.INVISIBLE
+                }
                 currentPage -= 1
                 viewPager.setCurrentItem(currentPage, true)
+                changeCircleColor(MOVE_PREVIOUS_PAGE)
             }
         }
+    }
 
+    private fun moveNext() {
         nextButton.setOnClickListener {
             if (currentPage == SimpleFragmentPagerAdapter.USER_INFO_3) {
-                nextButton.text = "완료"
+                nextButton.text = getText(R.string.user_info_done)
             }
             if (currentPage < SimpleFragmentPagerAdapter.USER_INFO_4) {
                 if (currentPage == SimpleFragmentPagerAdapter.USER_INFO_1) {
@@ -55,8 +86,20 @@ class UserInfoActivity : AppCompatActivity() {
                 }
                 currentPage += 1
                 viewPager.setCurrentItem(currentPage, true)
+                changeCircleColor(MOVE_NEXT_PAGE)
             }
         }
+    }
+
+    private fun changeCircleColor(doesMovePrevious: Boolean) {
+        val previousPage: Int =
+                if (doesMovePrevious) {
+                    currentPage + 1
+                } else {
+                    currentPage - 1
+                }
+        circleImageViewArrayList[previousPage].setImageResource(R.drawable.ic_default_circle)
+        circleImageViewArrayList[currentPage].setImageResource(R.drawable.ic_pink_circle)
     }
 
     fun getViewPager(): ViewPager {
@@ -65,5 +108,7 @@ class UserInfoActivity : AppCompatActivity() {
 
     companion object {
         const val MAX_PAGE_SIZE = 4
+        const val MOVE_PREVIOUS_PAGE = true
+        const val MOVE_NEXT_PAGE = false
     }
 }
