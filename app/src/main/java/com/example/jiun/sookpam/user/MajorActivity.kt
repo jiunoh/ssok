3b7785c68972453ff2cdb1b82a20b7d9c6c1eed5
Hyemin Kim <1117hyemin@gmail.com>
2018-02-19 21:00:26 +0900

    Create activity for selecting major

---
+++ app/src/main/java/com/example/jiun/sookpam/user/MajorActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jiun.sookpam.user
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jiun.sookpam.R
+
+class MajorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_major)
+    }
+}
