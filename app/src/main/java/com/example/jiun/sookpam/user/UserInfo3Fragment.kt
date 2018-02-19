8ce73097a4e3b0269ab2955a90724f8696680bba
Hyemin Kim <1117hyemin@gmail.com>
2018-02-19 16:24:50 +0900

    Refactor page3, 4

--- app/src/main/java/com/example/jiun/sookpam/user/UserInfo3Fragment.kt
+++ app/src/main/java/com/example/jiun/sookpam/user/UserInfo3Fragment.kt
@@ -2,34 +2,36 @@ package com.example.jiun.sookpam.user
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
 import android.view.*
 import android.widget.Button
-import android.widget.Toast
 import com.example.jiun.sookpam.R
-import com.example.jiun.sookpam.util.SharedPreferenceUtil
 import kotlinx.android.synthetic.main.fragment_user_info3.*
-
 
 class UserInfo3Fragment : Fragment() {
     private var userInfo3View: View? = null
     private var userInfo3Activity: Activity? = null
     private var userInfo3Context: Context? = null
+    private var personalCategory: PersonalCategory? = null
     private var detailButtons: ArrayList<Button> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         userInfo3View = inflater.inflate(R.layout.fragment_user_info3, container, false)
         userInfo3Activity = activity
         userInfo3Context = userInfo3View!!.context
+        personalCategory = PersonalCategory(userInfo3Context!!, PersonalCategory.PAGE3)
         return userInfo3View
     }
 
     override fun onStart() {
         super.onStart()
         initializeDetailButtons()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        personalCategory!!.setColorsOf(detailButtons)
     }
 
     private fun initializeDetailButtons() {
@@ -43,59 +45,8 @@ class UserInfo3Fragment : Fragment() {
 
         for (button in detailButtons) {
             button.setOnClickListener {
-                setCategoryButtonListener(button)
-            }
-            getColorSet(button)
-        }
-    }
-
-    private fun setCategoryButtonListener(button: Button) {
-        val categoryName = button.text.toString()
-        val currentKey = SharedPreferenceUtil.get(userInfo3Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-        when (currentKey) {
-            PersonalCategory.NORMAL_CATEGORY -> {
-                SharedPreferenceUtil.set(userInfo3Context, categoryName, PersonalCategory.INTEREST_CATEGORY)
-                changeButtonColor(button)
-
-            }
-            PersonalCategory.INTEREST_CATEGORY -> {
-                SharedPreferenceUtil.set(userInfo3Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-                changeButtonColor(button)
-            }
-            PersonalCategory.UNINTEREST_CATEGORY -> {
-                Toast.makeText(userInfo3Context, getString(R.string.user_info3_uninterest_already_checked)
-                        , Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun changeButtonColor(button: Button) {
-        if (button.currentTextColor
-                == ContextCompat.getColor(userInfo3Context!!, R.color.colorPrimary)) {
-            button.setBackgroundResource(R.drawable.circle_shape_blue)
-            button.setTextColor(Color.WHITE)
-        } else {
-            button.setBackgroundResource(R.drawable.circle_shape_white_blue)
-            button.setTextColor(ContextCompat.getColor(userInfo3Context!!, R.color.colorPrimary))
-        }
-    }
-
-    private fun getColorSet(button: Button) {
-        val categoryName = button.text.toString()
-        val categoryStatus = SharedPreferenceUtil
-                .get(userInfo3Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-        when (categoryStatus) {
-            PersonalCategory.NORMAL_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_white_blue)
-                button.setTextColor(ContextCompat.getColor(userInfo3Context!!, R.color.colorPrimary))
-            }
-            PersonalCategory.INTEREST_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_blue)
-                button.setTextColor(Color.WHITE)
-            }
-            PersonalCategory.UNINTEREST_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_gray)
-                button.setTextColor(Color.WHITE)
+                personalCategory!!.setCategoryButtonListener(button
+                        , getString(R.string.user_info3_uninterest_already_checked))
             }
         }
     }
--- app/src/main/java/com/example/jiun/sookpam/user/UserInfo4Fragment.kt
+++ app/src/main/java/com/example/jiun/sookpam/user/UserInfo4Fragment.kt
@@ -18,18 +18,25 @@ class UserInfo4Fragment : Fragment() {
     private var userInfo4View: View? = null
     private var userInfo4Activity: Activity? = null
     private var userInfo4Context: Context? = null
+    private var personalCategory:PersonalCategory?=null
     private var detailButtons: ArrayList<Button> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         userInfo4View = inflater.inflate(R.layout.fragment_user_info4, container, false)
         userInfo4Activity = activity
         userInfo4Context = userInfo4View!!.context
+        personalCategory = PersonalCategory(userInfo4Context!!, PersonalCategory.PAGE4)
         return userInfo4View
     }
 
     override fun onStart() {
         super.onStart()
         initializeDetailButtons()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        personalCategory!!.setColorsOf(detailButtons)
     }
 
     private fun initializeDetailButtons() {
@@ -43,58 +50,8 @@ class UserInfo4Fragment : Fragment() {
 
         for (button in detailButtons) {
             button.setOnClickListener {
-                setCategoryButtonListener(button)
-            }
-            getColorSet(button)
-        }
-    }
-
-    private fun changeButtonColor(button: Button) {
-        if (button.currentTextColor
-                == ContextCompat.getColor(userInfo4Context!!, R.color.colorDarkGray)) {
-            button.setBackgroundResource(R.drawable.circle_shape_dark_gray)
-            button.setTextColor(Color.WHITE)
-        } else {
-            button.setBackgroundResource(R.drawable.circle_shape_white_dark_gray)
-            button.setTextColor(ContextCompat.getColor(userInfo4Context!!, R.color.colorDarkGray))
-        }
-    }
-
-    private fun setCategoryButtonListener(button: Button) {
-        val categoryName = button.text.toString()
-        val currentKey = SharedPreferenceUtil.get(userInfo4Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-        when (currentKey) {
-            PersonalCategory.NORMAL_CATEGORY -> {
-                SharedPreferenceUtil.set(userInfo4Context, categoryName, PersonalCategory.UNINTEREST_CATEGORY)
-                changeButtonColor(button)
-            }
-            PersonalCategory.UNINTEREST_CATEGORY -> {
-                SharedPreferenceUtil.set(userInfo4Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-                changeButtonColor(button)
-            }
-            PersonalCategory.INTEREST_CATEGORY -> {
-                Toast.makeText(userInfo4Context, getString(R.string.user_info4_interest_already_checked)
-                        , Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun getColorSet(button: Button) {
-        val categoryName = button.text.toString()
-        val categoryStatus = SharedPreferenceUtil
-                .get(userInfo4Context, categoryName, PersonalCategory.NORMAL_CATEGORY)
-        when (categoryStatus) {
-            PersonalCategory.NORMAL_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_white_dark_gray)
-                button.setTextColor(ContextCompat.getColor(userInfo4Context!!, R.color.colorDarkGray))
-            }
-            PersonalCategory.INTEREST_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_gray)
-                button.setTextColor(Color.WHITE)
-            }
-            PersonalCategory.UNINTEREST_CATEGORY -> {
-                button.setBackgroundResource(R.drawable.circle_shape_dark_gray)
-                button.setTextColor(Color.WHITE)
+                personalCategory!!.setCategoryButtonListener(button
+                        , getString(R.string.user_info4_interest_already_checked))
             }
         }
     }
