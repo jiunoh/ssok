83a35369a9510bb98186b41314df5f20729a0829
jiun <juneoh227@gmail.com>
2018-02-21 13:54:04 +0900

    created viewpager main

--- app/src/main/java/com/example/jiun/sookpam/ViewPagerMainActivity.java
+++ app/src/main/java/com/example/jiun/sookpam/ViewPagerMainActivity.java
@@ -1,6 +1,12 @@
 package com.example.jiun.sookpam;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class ViewPagerMainActivity extends AppCompatActivity {
@@ -9,5 +15,70 @@ public class ViewPagerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager_main);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager_main);
+        TabLayout upperTabs = (TabLayout) findViewById(R.id.upper_tab_layout);
+        upperTabs.setupWithViewPager(viewPager);
+
+        MessageFragAdapter messageFragAdapter = new MessageFragAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(messageFragAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(upperTabs));
+        upperTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private class MessageFragAdapter extends FragmentStatePagerAdapter {
+        private final int TAB_NUMBER = 3;
+
+        public MessageFragAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                case 1:
+                case 2:
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_NUMBER;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "추천";
+                case 1:
+                    return "공통";
+                case 2:
+                    return "학부/학과";
+                default:
+                    return null;
+            }
+        }
     }
 }
