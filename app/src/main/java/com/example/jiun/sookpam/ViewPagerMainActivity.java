0cbb6290d41c3f3fa5cdfcb47663262c9de3c367
jiun <juneoh227@gmail.com>
2018-02-22 17:25:45 +0900

    created toolbar

--- app/src/main/java/com/example/jiun/sookpam/ViewPagerMainActivity.java
+++ app/src/main/java/com/example/jiun/sookpam/ViewPagerMainActivity.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ViewPagerMainActivity extends AppCompatActivity {
+    Toolbar vpToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager_main);
+
+        vpToolbar = (Toolbar) findViewById(R.id.view_pager_toolbar);
+        setSupportActionBar(vpToolbar);
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager_main);
         TabLayout upperTabs = (TabLayout) findViewById(R.id.upper_tab_layout);
