bddffe60d6b1fd558cc6bcfbe4e39a7f130fd39d
jiun <juneoh227@gmail.com>
2018-02-24 18:38:02 +0900

    removed auto-created methods and comments

--- app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
@@ -1,58 +1,31 @@
 package com.example.jiun.sookpam;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.jiun.sookpam.model.RecordDBManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MessageCommonFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MessageCommonFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class MessageCommonFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private RecordDBManager categoryManager;
 
     public MessageCommonFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment MessageCommonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     public static MessageCommonFragment newInstance() {
         MessageCommonFragment fragment = new MessageCommonFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,25 +35,5 @@ public class MessageCommonFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_message_common, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
