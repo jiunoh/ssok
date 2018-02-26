0c8dcbcc9f8ff476845049fda2763fcfa47c0748
Hyemin Kim <1117hyemin@gmail.com>
2018-02-26 15:13:54 +0900

    Create files for cilent-server communication

---
+++ app/src/main/java/com/example/jiun/sookpam/server/ServerActivity.kt
@@ -0,0 +1,13 @@
+package com.example.jiun.sookpam.server
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jiun.sookpam.R
+
+class ServerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server)
+    }
+}
