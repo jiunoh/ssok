cae188dc1775661b96e261280ad88e28a72dccac
jiun <juneoh227@gmail.com>
2018-02-28 05:24:46 +0900

    tried custom view (crashes)

--- app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -22,18 +23,24 @@ public class MessageCommonFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message_common, container, false);
+        RelativeLayout layoutContainer = view.findViewById(R.id.message_common_container);
+        int viewWidth = view.getMeasuredWidth();
+        int viewHeight = view.getMeasuredHeight();
 
         TextView category1 = view.findViewById(R.id.category_1);
-        TextView category1_1 = view.findViewById(R.id.category_1_content1);
-        TextView category1_2 = view.findViewById(R.id.category_1_content2);
-        TextView category1_3 = view.findViewById(R.id.category_1_content3);
-        TextView button1 = view.findViewById(R.id.category_1_button);
-
         category1.setText("장학");
-        category1_1.setText("아산장학재단 신청 안내");
-        category1_2.setText("근로장학생 모집");
-        category1_3.setText("외부장학금 안내");
-        button1.setText("MORE >");
+        category1.setBackgroundResource(R.drawable.category_shape);
+        category1.setWidth(viewWidth/3);
+        category1.setHeight(viewHeight/9);
+
+        TextView category2 = view.findViewById(R.id.category_2);
+        category2.setText("학사");
+        category2.setBackgroundResource(R.drawable.category_shape);
+        category2.setWidth(viewWidth/3);
+        category2.setHeight(viewHeight/9);
+
+        layoutContainer.addView(category1);
+        layoutContainer.addView(category2);
 
         // Inflate the layout for this fragment
         return view;
