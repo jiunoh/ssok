b32a9dbdcd0a631023ae30fa2822f3807f88772d
jiun <juneoh227@gmail.com>
2018-02-28 02:59:03 +0900

    created web_recommendation

--- app/src/main/java/com/example/jiun/sookpam/WebRecomFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/WebRecomFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 /**
@@ -26,10 +29,23 @@ public class WebRecomFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_recom, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_recom, container, false);
+        MessageDepartListAdapter adapter = new MessageDepartListAdapter();
+        ListView listView = view.findViewById(R.id.web_recom_listview);
+        listView.setAdapter(adapter);
+
+        adapter.addItem("demo web title", "demo");
+        adapter.addItem("demo web title", "demo");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity().getApplicationContext(), "리스트 아이템 클릭됨", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return view;
     }
 
 }
