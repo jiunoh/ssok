e43784c42363d465bad5118f5b7643f1fc99586e
jiun <juneoh227@gmail.com>
2018-02-28 00:22:07 +0900

    message_common demo view

--- app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/MessageCommonFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class MessageCommonFragment extends Fragment {
@@ -20,10 +21,22 @@ public class MessageCommonFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        
+        View view = inflater.inflate(R.layout.fragment_message_common, container, false);
+
+        TextView category1 = view.findViewById(R.id.category_1);
+        TextView category1_1 = view.findViewById(R.id.category_1_content1);
+        TextView category1_2 = view.findViewById(R.id.category_1_content2);
+        TextView category1_3 = view.findViewById(R.id.category_1_content3);
+        TextView button1 = view.findViewById(R.id.category_1_button);
+
+        category1.setText("장학");
+        category1_1.setText("아산장학재단 신청 안내");
+        category1_2.setText("근로장학생 모집");
+        category1_3.setText("외부장학금 안내");
+        button1.setText("MORE >");
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message_common, container, false);
+        return view;
     }
 
     public static MessageCommonFragment newInstance() {
--- app/src/main/java/com/example/jiun/sookpam/MessageDepartFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/MessageDepartFragment.java
@@ -42,7 +42,6 @@ public class MessageDepartFragment extends Fragment {
             }
         });
 
-
         return view;
     }
 
