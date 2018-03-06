8797726ec269d3115bad1db94336aa053e937a97
lyn <lync2846@gmail.com>
2018-03-06 23:37:19 +0900

    add toolbar for searchview due to design change

--- app/src/main/java/com/example/jiun/sookpam/searchable/SearchableActivity.kt
+++ app/src/main/java/com/example/jiun/sookpam/searchable/SearchableActivity.kt
@@ -6,8 +6,13 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
-import android.widget.SearchView
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.View
+import android.support.v7.widget.SearchView
 import com.example.jiun.sookpam.RecyclerItemClickListener
 import com.example.jiun.sookpam.message.ContentActivity
 import com.example.jiun.sookpam.message.ContentItem
@@ -18,7 +23,8 @@ import kotlinx.android.synthetic.main.activity_searchable.*
 import java.util.ArrayList
 
 
-class SearchableActivity : AppCompatActivity(), SearchView.OnQueryTextListener, SearchView.OnCloseListener {
+class SearchableActivity : AppCompatActivity(), SearchView.OnCloseListener {
+    private lateinit var toolbar: Toolbar
     private lateinit var responseList: ArrayList<SearchItem>
     private lateinit var editsearch: SearchView
     private lateinit var adapter: SearchableRecyclerAdapter
@@ -28,41 +34,59 @@ class SearchableActivity : AppCompatActivity(), SearchView.OnQueryTextListener, 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchable)
         responseList = ArrayList<SearchItem>()
+        setToolbar()
         setRecyclerView()
-        editsearch = search_view
-        editsearch.setOnQueryTextListener(this);
 
         if (isNetWork()) {
         } else {
             //error
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val searchItem = menu!!.findItem(R.id.action_search)
+        editsearch = MenuItemCompat.getActionView(searchItem) as SearchView
+        editsearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                adapter.filter(query)
+                editsearch.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        });
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setToolbar() {
+        toolbar = search_toolbar
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+        toolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_ab_back_material);
+        toolbar.setNavigationOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                finish()
+            }
+        })
+    }
 
     private fun isNetWork(): Boolean {
         var connectManager: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        var activeNetwork: NetworkInfo? = connectManager.getActiveNetworkInfo()
-        var isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+        var activeNetwork: NetworkInfo? = connectManager.activeNetworkInfo
+        var isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting;
         return isConnected
     }
 
     private fun setRecyclerView() {
         adapter = SearchableRecyclerAdapter(responseList)
         search_recycler_view.adapter = adapter
-
         search_recycler_view.addOnItemTouchListener(RecyclerItemClickListener(this,
                 RecyclerItemClickListener.OnItemClickListener { view, position ->
                     val data = responseList.get(position)
                     showMessageBody(data)
                 }))
-    }
-
-    override fun onQueryTextSubmit(query: String?): Boolean {
-        adapter.filter(query)
-        return false
-    }
-
-    override fun onQueryTextChange(newText: String?): Boolean {
-        return false
     }
 
     override fun onClose(): Boolean {
