7599c1468b51bd308cc53c41fc1adf15cc470daf
Hyemin Kim <1117hyemin@gmail.com>
2018-03-07 01:40:49 +0900

    Apply internet connecting error image and text

--- app/src/main/java/com/example/jiun/sookpam/web/recommend/WebRecommendFragment.kt
+++ app/src/main/java/com/example/jiun/sookpam/web/recommend/WebRecommendFragment.kt
@@ -1,6 +1,5 @@
 package com.example.jiun.sookpam.web.recommend
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -9,6 +8,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import com.example.jiun.sookpam.R
 import com.example.jiun.sookpam.RecyclerItemClickListener
 import com.example.jiun.sookpam.UIAnimation
@@ -25,6 +27,8 @@ import retrofit2.Response
 class WebRecommendFragment : Fragment() {
     private lateinit var service: RecordService
     private lateinit var webRecommendRecyclerView: RecyclerView
+    private lateinit var connectErrorLinearLayout: LinearLayout
+    private lateinit var connectErrorTextView: TextView
     private var records: List<RecordResponse>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -37,6 +41,8 @@ class WebRecommendFragment : Fragment() {
     }
 
     private fun initialize() {
+        connectErrorLinearLayout = web_recommend_error_linear
+        connectErrorTextView = web_recommend_error_txt
         webRecommendRecyclerView = web_recommend_recycler_view
         webRecommendRecyclerView.layoutManager = LinearLayoutManager(context)
         webRecommendRecyclerView.addOnItemTouchListener(RecyclerItemClickListener(context, RecyclerItemClickListener.OnItemClickListener { _, position ->
@@ -65,6 +71,7 @@ class WebRecommendFragment : Fragment() {
                 userInformation.interestStudent
         ).enqueue(object : Callback<List<RecordResponse>> {
             override fun onFailure(call: Call<List<RecordResponse>>?, t: Throwable?) {
+                showInternetConnectionError()
             }
 
             override fun onResponse(call: Call<List<RecordResponse>>?, response: Response<List<RecordResponse>>?) {
@@ -75,6 +82,13 @@ class WebRecommendFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun showInternetConnectionError() {
+        Toast.makeText(context, getString(R.string.internet_connect_error), Toast.LENGTH_SHORT).show()
+        webRecommendRecyclerView.visibility = View.INVISIBLE
+        connectErrorLinearLayout.visibility = View.VISIBLE
+        connectErrorTextView.text = getString(R.string.internet_connect_error)
     }
 
     companion object {
