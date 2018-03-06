d6394b74921884c4e7a4f527bcf26707edd1a907
Hyemin Kim <1117hyemin@gmail.com>
2018-03-07 02:12:46 +0900

    When user click refresh button then reload recommend data

--- app/src/main/java/com/example/jiun/sookpam/web/recommend/WebRecommendFragment.kt
+++ app/src/main/java/com/example/jiun/sookpam/web/recommend/WebRecommendFragment.kt
@@ -3,32 +3,27 @@ package com.example.jiun.sookpam.web.recommend
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.*
+import android.view.*
+import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.example.jiun.sookpam.R
-import com.example.jiun.sookpam.RecyclerItemClickListener
-import com.example.jiun.sookpam.UIAnimation
-import com.example.jiun.sookpam.server.ApiUtils
-import com.example.jiun.sookpam.server.RecordResponse
-import com.example.jiun.sookpam.server.RecordService
+import com.example.jiun.sookpam.*
+import com.example.jiun.sookpam.server.*
 import com.example.jiun.sookpam.user.UserInformation
 import com.example.jiun.sookpam.web.WebContentActivity
+import kotlinx.android.synthetic.main.activity_view_pager_main.*
 import kotlinx.android.synthetic.main.fragment_web_recommend.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import retrofit2.*
 
 class WebRecommendFragment : Fragment() {
     private lateinit var service: RecordService
     private lateinit var webRecommendRecyclerView: RecyclerView
     private lateinit var connectErrorLinearLayout: LinearLayout
     private lateinit var connectErrorTextView: TextView
+    private lateinit var refreshImageButton: ImageButton
     private var records: List<RecordResponse>? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -52,8 +47,7 @@ class WebRecommendFragment : Fragment() {
         }))
         service = ApiUtils.getRecordService()
         val userInformation = UserInformation(context!!)
-        service.getRecommendRecords(
-                userInformation.studentGrade,
+        loadData(userInformation.studentGrade,
                 userInformation.studentYear,
                 userInformation.major1,
                 userInformation.major2,
@@ -68,8 +62,39 @@ class WebRecommendFragment : Fragment() {
                 userInformation.interestSystem,
                 userInformation.interestGlobal,
                 userInformation.interestCareer,
-                userInformation.interestStudent
-        ).enqueue(object : Callback<List<RecordResponse>> {
+                userInformation.interestStudent)
+        refreshImageButton = activity!!.main_refresh_image_btn
+        refreshImageButton.setOnClickListener {
+            val rotateAnimation = UIAnimation.setRotateAnimation(refreshImageButton)
+            refreshImageButton.startAnimation(rotateAnimation)
+            loadData(userInformation.studentGrade,
+                    userInformation.studentYear,
+                    userInformation.major1,
+                    userInformation.major2,
+                    userInformation.schoolScholar,
+                    userInformation.governmentScholar,
+                    userInformation.externalScholar,
+                    userInformation.studentStatus,
+                    userInformation.interestScholarship,
+                    userInformation.interestAcademic,
+                    userInformation.interestEntrance,
+                    userInformation.interestRecruit,
+                    userInformation.interestSystem,
+                    userInformation.interestGlobal,
+                    userInformation.interestCareer,
+                    userInformation.interestStudent)
+        }
+    }
+
+    private fun loadData(studentGrade: Int, studentYear: Int, major1: String, major2: String,
+                         schoolScholar: Boolean, governmentScholar: Boolean, externalScholar: Boolean,
+                         studentStatus: Boolean, interestScholarship: Int, interestAcademic: Int,
+                         interestEntrance: Int, interestRecruit: Int, interestSystem: Int,
+                         interestGlobal: Int, interestCareer: Int, interestStudent: Int) {
+        service.getRecommendRecords(studentGrade, studentYear, major1, major2, schoolScholar,
+                governmentScholar, externalScholar, studentStatus, interestScholarship, interestAcademic,
+                interestEntrance, interestRecruit, interestSystem, interestGlobal, interestCareer,
+                interestStudent).enqueue(object : Callback<List<RecordResponse>> {
             override fun onFailure(call: Call<List<RecordResponse>>?, t: Throwable?) {
                 showInternetConnectionError()
             }
