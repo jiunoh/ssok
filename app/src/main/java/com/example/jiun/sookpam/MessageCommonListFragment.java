a8084f0b0f110b4d61f22099e652cef4c6bd3d52
jiun <juneoh227@gmail.com>
2018-03-07 18:58:25 +0900

    created new frag

---
+++ app/src/main/java/com/example/jiun/sookpam/MessageCommonListFragment.java
@@ -0,0 +1,36 @@
+package com.example.jiun.sookpam;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MessageCommonListFragment extends Fragment {
+
+    public MessageCommonListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_message_common_list, container, false);
+
+        return view;
+    }
+
+    public static MessageCommonListFragment newInstance() {
+        MessageCommonListFragment fragment = new MessageCommonListFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
