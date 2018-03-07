ee7f02aec790e79494add17939133093b59f7824
jiun <juneoh227@gmail.com>
2018-03-07 19:06:33 +0900

    deleted auto-created factory methods

--- app/src/main/java/com/example/jiun/sookpam/MessageDepartFragment.java
+++ app/src/main/java/com/example/jiun/sookpam/MessageDepartFragment.java
@@ -1,7 +1,6 @@
 package com.example.jiun.sookpam;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,7 +23,6 @@ import io.realm.Realm;
 
 
 public class MessageDepartFragment extends Fragment {
-    private OnFragmentInteractionListener mListener;
     private RecordDBManager categoryManager;
 
     public MessageDepartFragment() {
@@ -107,27 +105,5 @@ public class MessageDepartFragment extends Fragment {
         bundle.putSerializable("OBJECT", contentItem);
         intent.putExtras(bundle);
         startActivity(intent);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
