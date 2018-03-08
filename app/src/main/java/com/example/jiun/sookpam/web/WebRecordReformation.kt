8474f8d5c6cf7634086080e081a18f67d82f294e
Hyemin Kim <1117hyemin@gmail.com>
2018-03-09 01:54:57 +0900

    Remove unnecessary space from title

--- app/src/main/java/com/example/jiun/sookpam/web/WebRecordReformation.kt
+++ app/src/main/java/com/example/jiun/sookpam/web/WebRecordReformation.kt
@@ -4,10 +4,14 @@ class WebRecordReformation {
     companion object {
         fun getTitleSubstring(title: String): String {
             val titleChars = title.toCharArray()
-            var resultTitle:String = title
+            var resultTitle: String = title
             for ((index, character) in titleChars.withIndex()) {
                 if (character == ']') {
-                    resultTitle = title.substring(index+1, title.lastIndex+1)
+                    resultTitle = if (titleChars[index + 1] == ' ') {
+                        title.substring(index + 2, title.lastIndex + 1)
+                    } else {
+                        title.substring(index + 1, title.lastIndex + 1)
+                    }
                     break
                 }
             }
