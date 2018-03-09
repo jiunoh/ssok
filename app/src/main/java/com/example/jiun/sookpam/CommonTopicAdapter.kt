64710b2f1af051fd4dd37e87d9da5753106350e2
Hyemin Kim <1117hyemin@gmail.com>
2018-03-09 14:48:08 +0900

    Apply topic recycler view to common message

--- app/src/main/java/com/example/jiun/sookpam/CommonTopicAdapter.kt
+++ app/src/main/java/com/example/jiun/sookpam/CommonTopicAdapter.kt
@@ -8,14 +8,14 @@ class CommonTopicAdapter {
     companion object {
         private const val INTEREST = "INTEREST"
         private const val NORMAL = "NORMAL"
-        private const val SCHOLAR_DETAIL = "교내외 장학 소식을 제공"
-        private const val ACADEMIC_DETAIL = "학사일정 및 교내 주요 소식을 제공"
-        private const val EVENT_DETAIL = "교내 다양한 행사 소식을 제공"
-        private const val RECRUIT_DETAIL = "교내인턴, 동아리 회원 모집 등의 소식을 제공"
-        private const val SYSTEM_DETAIL = "교내 IT 서비스 소식을 제공"
-        private const val GLOBAL_DETAIL = "국제협력팀의 소식을 제공"
-        private const val CAREER_DETAIL = "취업 관련 교내 행사 및 외부 소식을 제공"
-        private const val STUDENT_DETAIL = "학생지원팀의 소식을 제공"
+        private const val SCHOLAR_DETAIL = "교내외 장학 소식을 제공합니다"
+        private const val ACADEMIC_DETAIL = "학사일정 및 교내 주요 소식을 제공합니다"
+        private const val EVENT_DETAIL = "교내 다양한 행사 소식을 제공합니다"
+        private const val RECRUIT_DETAIL = "교내인턴, 동아리 회원모집 등의 소식을 제공합니다"
+        private const val SYSTEM_DETAIL = "교내 IT 서비스 소식을 제공합니다"
+        private const val GLOBAL_DETAIL = "국제협력팀의 소식을 제공합니다"
+        private const val CAREER_DETAIL = "취업 관련 교내 행사 및 외부 소식을 제공합니다"
+        private const val STUDENT_DETAIL = "학생지원팀의 소식을 제공합니다"
 
         fun getInterestOrNormalTopics(context: Context): List<CommonTopic> {
             val interestTopics = getTopics(context, INTEREST, 1)
