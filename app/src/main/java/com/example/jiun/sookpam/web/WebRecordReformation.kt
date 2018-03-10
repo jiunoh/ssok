d0c9208498773c78fa216ca0818a64a7d3ff4533
Hyemin Kim <1117hyemin@gmail.com>
2018-03-11 05:00:29 +0900

    Add condition to web record reformation when category is '취업'

--- app/src/main/java/com/example/jiun/sookpam/web/WebRecordReformation.kt
+++ app/src/main/java/com/example/jiun/sookpam/web/WebRecordReformation.kt
@@ -3,7 +3,8 @@ package com.example.jiun.sookpam.web
 class WebRecordReformation {
     companion object {
         fun getTitleSubstring(title: String, category: String, division: String): String {
-            if (category != "공통") return title
+            if (category != "공통" && category != "취업")
+                return title
             else if (division == "국제") return title
             val titleChars = title.toCharArray()
             var resultTitle: String = title
