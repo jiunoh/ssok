7af08284f67d96865e9bf7cea76fd8c85bc90134
Hyemin Kim <1117hyemin@gmail.com>
2018-03-11 00:50:24 +0900

    Change custom linear layout manager code
    for removing system error log

--- app/src/main/java/com/example/jiun/sookpam/CustomLinearLayoutManager.kt
+++ app/src/main/java/com/example/jiun/sookpam/CustomLinearLayoutManager.kt
@@ -1,80 +1,75 @@
 package com.example.jiun.sookpam
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.view.ViewGroup
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 
-class CustomLinearLayoutManager(context: Context) : LinearLayoutManager(context) {
+/**
+ * Created by cym on 15/5/27.
+ */
+class CustomLinearLayoutManager(context: Context, orientation: Int, reverseLayout: Boolean) : LinearLayoutManager(context, orientation, reverseLayout) {
+    init {
+        isAutoMeasureEnabled = false
+    }
+
     private val mMeasuredDimension = IntArray(2)
 
-    @SuppressLint("SwitchIntDef")
-    override fun onMeasure(recycler: RecyclerView.Recycler?, state: RecyclerView.State?, widthSpec: Int, heightSpec: Int) {
-
+    override fun onMeasure(recycler: RecyclerView.Recycler?, state: RecyclerView.State?,
+                           widthSpec: Int, heightSpec: Int) {
         val widthMode = View.MeasureSpec.getMode(widthSpec)
         val heightMode = View.MeasureSpec.getMode(heightSpec)
         val widthSize = View.MeasureSpec.getSize(widthSpec)
         val heightSize = View.MeasureSpec.getSize(heightSpec)
-
         var width = 0
         var height = 0
         for (i in 0 until itemCount) {
-            measureScrapChild(recycler, i, View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
-                    mMeasuredDimension)
-
-
             if (orientation == LinearLayoutManager.HORIZONTAL) {
+                measureScrapChild(recycler, i,
+                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                        heightSpec,
+                        mMeasuredDimension)
+
                 width += mMeasuredDimension[0]
                 if (i == 0) {
                     height = mMeasuredDimension[1]
                 }
             } else {
-                height = height + mMeasuredDimension[1]
+                measureScrapChild(recycler, i,
+                        widthSpec,
+                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                        mMeasuredDimension)
+                height += mMeasuredDimension[1]
                 if (i == 0) {
                     width = mMeasuredDimension[0]
                 }
             }
         }
-        when (widthMode) {
-            View.MeasureSpec.EXACTLY -> width = widthSize
+        if (widthMode == View.MeasureSpec.EXACTLY) {
+            width = widthSize
         }
 
-        when (heightMode) {
-            View.MeasureSpec.EXACTLY -> height = heightSize
+        if (heightMode == View.MeasureSpec.EXACTLY) {
+            height = heightSize
         }
 
         setMeasuredDimension(width, height)
     }
 
-    private fun measureScrapChild(recycler: RecyclerView.Recycler?, position: Int, widthSpec: Int,
-                                  heightSpec: Int, measuredDimension: IntArray) {
-        try {
-            val view = recycler!!.getViewForPosition(0)
-
-            if (view != null) {
-                val p = view.layoutParams as RecyclerView.LayoutParams
-
-                val childWidthSpec = ViewGroup.getChildMeasureSpec(widthSpec,
-                        paddingLeft + paddingRight, p.width)
-
-                val childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec,
-                        paddingTop + paddingBottom, p.height)
-
-                view.measure(childWidthSpec, childHeightSpec)
-                measuredDimension[0] = view.measuredWidth + p.leftMargin + p.rightMargin
-                measuredDimension[1] = view.measuredHeight + p.bottomMargin + p.topMargin
-                recycler.recycleView(view)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
+    private fun measureScrapChild(recycler: RecyclerView.Recycler?, position: Int, widthSpec: Int, heightSpec: Int, measuredDimension: IntArray) {
+        val view = recycler!!.getViewForPosition(position)
+        recycler.bindViewToPosition(view, position)
+        if (view != null) {
+            val p = view.layoutParams as RecyclerView.LayoutParams
+            val childWidthSpec = ViewGroup.getChildMeasureSpec(widthSpec,
+                    paddingLeft + paddingRight, p.width)
+            val childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec,
+                    paddingTop + paddingBottom, p.height)
+            view.measure(childWidthSpec, childHeightSpec)
+            measuredDimension[0] = view.measuredWidth + p.leftMargin + p.rightMargin
+            measuredDimension[1] = view.measuredHeight + p.bottomMargin + p.topMargin
+            recycler.recycleView(view)
         }
-
-    }
-
-    companion object {
-        private val TAG = CustomLinearLayoutManager::class.java.simpleName
     }
 }
--- app/src/main/java/com/example/jiun/sookpam/user/major/CollegeRecyclerAdapter.kt
+++ app/src/main/java/com/example/jiun/sookpam/user/major/CollegeRecyclerAdapter.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.SparseBooleanArray
 import android.view.*
@@ -31,7 +32,7 @@ class CollegeRecyclerAdapter(val data: List<MajorItemModel>) : RecyclerView.Adap
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         context = parent!!.context
-        layoutManager = CustomLinearLayoutManager(context)
+        layoutManager = CustomLinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         val view = LayoutInflater.from(context).inflate(R.layout.college_recycler_item, parent, false)
         return ViewHolder(view)
     }
